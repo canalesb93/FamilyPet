21c6a256f4cc149b722c314f8db471944729329f
Ricardo Canales <canalesb93@gmail.com>
2015-08-06 02:07:10 -0500

    Added parse and started with facebook

--- Family Pet/AppDelegate.swift
+++ Family Pet/AppDelegate.swift
@@ -7,15 +7,26 @@
 //
 
 import UIKit
+import Parse
+import Bolts
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
-
+    
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        // [Optional] Power your app with Local Datastore. For more info, go to
+        // https://parse.com/docs/ios_guide#localdatastore/iOS
+        Parse.enableLocalDatastore()
+        
+        // Initialize Parse.
+        Parse.setApplicationId("DAszHT3GJ15w6BfeeejMArmtfZrA52El8qvrE5ll",
+            clientKey: "vktqM4mrC7bxNzTQXOHrEVaG9j6PhBe7AyLxtQpU")
+        
+        // [Optional] Track statistics around application opens.
+        PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
+
         return true
     }
 
