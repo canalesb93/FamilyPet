d1a1ea94eb4313b2a5082b005217c0639d078aac
Ricardo Canales <canalesb93@gmail.com>
2015-08-07 20:59:33 -0500

    Adds picker view for pets

--- Family Pet/AddReminderViewController.swift
+++ Family Pet/AddReminderViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 
-class AddReminderViewController: UIViewController {
+class AddReminderViewController: UIViewController, AKPickerViewDataSource, AKPickerViewDelegate  {
 
     var reminderType: ReminderType = ReminderType.Feed
     var reminderFrequency: ReminderFrequency = ReminderFrequency.Once
     var reminderQueue: ReminderQueue = ReminderQueue.Daily
+    
+    @IBOutlet var petPickerView: AKPickerView!
     
     @IBOutlet var typeControl: ADVSegmentedControl!
     @IBOutlet var frequencyControl: ADVSegmentedControl!
@@ -20,9 +22,12 @@ class AddReminderViewController: UIViewController {
     
     var delegate: ReminderScrollView!
     
+    let titles = ["Tokyo", "Kanagawa", "Osaka", "Aichi", "Saitama", "Chiba", "Hyogo", "Hokkaido", "Fukuoka", "Shizuoka"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Configure segment controls
         typeControl.items = ["feed", "play", "walk", "vet", "meds"]
         typeControl.selectedIndex = 0
         typeControl.addTarget(self, action: "typeValueChanged:", forControlEvents: .ValueChanged)
@@ -35,6 +40,22 @@ class AddReminderViewController: UIViewController {
         queueControl.items = ["daily", "weekly"]
         queueControl.selectedIndex = 0
         queueControl.addTarget(self, action: "queueValueChanged:", forControlEvents: .ValueChanged)
+        
+        // Configure pet Picker
+        self.petPickerView.delegate = self
+        self.petPickerView.dataSource = self
+        
+        self.petPickerView.font = UIFont(name: "HelveticaNeue-Light", size: 25)!
+        self.petPickerView.highlightedFont = UIFont(name: "HelveticaNeue", size: 25)!
+        self.petPickerView.textColor = UIColor(netHex: 0x4C4C4F)
+        self.petPickerView.highlightedTextColor = UIColor(netHex: 0x4C4C4F)
+        self.petPickerView.pickerViewStyle = .Wheel
+        self.petPickerView.maskDisabled = false
+        self.petPickerView.reloadData()
+        
+    }
+    
+    func loadData(){
         
     }
     
@@ -49,7 +70,15 @@ class AddReminderViewController: UIViewController {
     func queueValueChanged(sender: AnyObject?){
         reminderQueue = ReminderQueue(rawValue: queueControl.selectedIndex)!
     }
+    
+    func numberOfItemsInPickerView(pickerView: AKPickerView) -> Int {
+        return self.titles.count
+    }
 
+    func pickerView(pickerView: AKPickerView, titleForItem item: Int) -> String {
+        return self.titles[item]
+    }
+    
     @IBAction func save(sender: AnyObject) {
     }
     @IBAction func cancel(sender: AnyObject) {
